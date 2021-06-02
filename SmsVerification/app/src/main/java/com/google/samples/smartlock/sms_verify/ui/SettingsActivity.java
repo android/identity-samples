504f59439f129bc02c45d2fd76acf4842af37819
gfan <gfan@google.com>
2021-06-02 14:20:03 -0700

    Update AGP to 4.2.x for Java 11 compatibility purpose

--- SmsVerification/app/src/main/java/com/google/samples/smartlock/sms_verify/ui/SettingsActivity.java
+++ SmsVerification/app/src/main/java/com/google/samples/smartlock/sms_verify/ui/SettingsActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.support.v7.app.ActionBar;
+import androidx.appcompat.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.view.MenuItem;
 
