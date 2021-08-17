3fe697cc724c14fada38e3bf05e2ea825e024ba4
Shailen Tuli <shailentuli@google.com>
2021-08-16 19:04:02 -0700

    Added common dependencies and databinding
    Change-Id: Ia5026a5cc97d99cdfc5a96359bdb678b95ea1a45

--- OneTap/app/src/main/java/com/google/android/gms/identity/sample/onetap/MainActivity.kt
+++ OneTap/app/src/main/java/com/google/android/gms/identity/sample/onetap/MainActivity.kt
@@ -17,10 +17,14 @@ package com.google.android.gms.identity.sample.onetap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.identity.sample.onetap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
