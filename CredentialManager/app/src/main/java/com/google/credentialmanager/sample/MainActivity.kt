f9e6a1c21787f0d45a819d3a663db1a037f82273
Niharika Arora <aroraniharika@google.com>
2023-04-16 02:42:39 +0530

    Updated Credential Manager sample app codebase with mock server implementation
    Change-Id: Id8f25a88d1d7638b867a885c0516131b6e344c10

--- CredentialManager/app/src/main/java/com/google/credentialmanager/sample/MainActivity.kt
+++ CredentialManager/app/src/main/java/com/google/credentialmanager/sample/MainActivity.kt
@@ -17,21 +17,70 @@
 package com.google.credentialmanager.sample
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import com.google.credentialmanager.sample.ui.CredentialManagerApp
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.credentialmanager.sample.HomeFragment.HomeFragmentCallback
+import com.google.credentialmanager.sample.MainFragment.MainFragmentCallback
+import com.google.credentialmanager.sample.R.id
+import com.google.credentialmanager.sample.SignInFragment.SignInFragmentCallback
+import com.google.credentialmanager.sample.SignUpFragment.SignUpFragmentCallback
+import com.google.credentialmanager.sample.databinding.ActivityMainBinding
 
-@AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), MainFragmentCallback, HomeFragmentCallback,
+    SignInFragmentCallback, SignUpFragmentCallback {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CredentialManagerApp()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Utils.initSharedPref(this)
+
+        if (Utils.isSignedIn()) {
+            showHome()
+        } else {
+            loadMainFragment()
+        }
+    }
+
+    override fun signup() {
+        loadFragment(SignUpFragment(), false)
+    }
+
+    override fun signIn() {
+        loadFragment(SignInFragment(), false)
+    }
+
+    override fun logout() {
+        supportFragmentManager.popBackStack("home", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        loadMainFragment()
+    }
+
+    private fun loadMainFragment() {
+        supportFragmentManager.popBackStack();
+        loadFragment(MainFragment(), false)
+    }
+
+    override fun showHome() {
+        supportFragmentManager.popBackStack()
+        loadFragment(HomeFragment(), true, "home")
+    }
+
+    private fun loadFragment(fragment: Fragment, flag: Boolean, backstackString: String? = null) {
+        Utils.configureSignedInPref(flag)
+        supportFragmentManager.beginTransaction().replace(id.fragment_container, fragment)
+            .addToBackStack(backstackString).commit()
+    }
+
+    override fun onBackPressed() {
+        if (Utils.isSignedIn() || supportFragmentManager.backStackEntryCount == 1) {
+            finish()
+        } else {
+            super.onBackPressed();
         }
     }
 }
-
-
-
---
+++ CredentialManager/app/src/main/java/com/google/credentialmanager/sample/SignInFragment.kt
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.credentialmanager.sample
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetPasswordOption
+import androidx.credentials.GetPublicKeyCredentialOption
+import androidx.credentials.PasswordCredential
+import androidx.credentials.PublicKeyCredential
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.credentialmanager.sample.databinding.FragmentSignInBinding
+import kotlinx.coroutines.launch
+
+class SignInFragment : Fragment() {
+
+    private lateinit var mCredMan: CredentialManager
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var listener: SignInFragmentCallback
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as SignInFragmentCallback
+        } catch (castException: ClassCastException) {
+            /** The activity does not implement the listener.  */
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mCredMan = CredentialManager.create(requireActivity())
+
+        binding.signIn.setOnClickListener(signInWithPassword())
+
+        binding.signInWithPasskeys.setOnClickListener(signInWithPasskeys())
+    }
+
+    private fun signInWithPasskeys(): View.OnClickListener {
+        return View.OnClickListener {
+
+            lifecycleScope.launch {
+                configureViews(View.VISIBLE, false)
+
+                val data = getPasskey()
+
+                configureViews(View.INVISIBLE, true)
+
+                data?.let {
+                    sendSignInResponseToServer()
+                    listener.showHome()
+                }
+            }
+        }
+    }
+
+    private fun configureViews(visibility: Int, flag: Boolean) {
+        configureProgress(visibility)
+        binding.signIn.isEnabled = flag
+        binding.signInWithPasskeys.isEnabled = flag
+    }
+
+    private fun configureProgress(visibility: Int) {
+        binding.textProgress.visibility = visibility
+        binding.circularProgressIndicator.visibility = visibility
+    }
+
+    private fun signInWithPassword(): View.OnClickListener {
+        return View.OnClickListener {
+            if (binding.username.text.isNullOrEmpty()) {
+                binding.username.error = "User name required"
+                binding.username.requestFocus()
+            } else if (binding.password.text.isNullOrEmpty()) {
+                binding.password.error = "Password required"
+                binding.password.requestFocus()
+            } else {
+                configureViews(View.VISIBLE, false)
+
+                simulateServerDelay()
+            }
+        }
+    }
+
+    private fun simulateServerDelay() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            Utils.setSignedInThroughPasskeys(false)
+            configureViews(View.INVISIBLE, true)
+
+            listener.showHome()
+        }, 2000)
+    }
+
+    private fun fetchAuthJsonFromServer(): String {
+        return Utils.readFromAsset(context, "AuthFromServer")
+    }
+
+    private fun sendSignInResponseToServer(): Boolean {
+        return true
+    }
+
+    private suspend fun getPasskey(): String? {
+        val requestJson = fetchAuthJsonFromServer()
+        val getPublicKeyCredentialOption = GetPublicKeyCredentialOption(requestJson, true)
+        val getPasswordOption = GetPasswordOption()
+        try {
+            val result = mCredMan.getCredential(
+                GetCredentialRequest(
+                    listOf(
+                        getPublicKeyCredentialOption,
+                        getPasswordOption
+                    )
+                ), requireActivity()
+            )
+
+            if (result.credential is PublicKeyCredential) {
+                val cred = result.credential as PublicKeyCredential
+                Utils.setSignedInThroughPasskeys(true)
+                return "Passkey: ${cred.authenticationResponseJson}"
+            }
+            if (result.credential is PasswordCredential) {
+                val cred = result.credential as PasswordCredential
+                Utils.setSignedInThroughPasskeys(false)
+                return "Got Password - User:${cred.id} Password: ${cred.password}"
+            }
+        } catch (e: Exception) {
+            configureViews(View.INVISIBLE, true)
+            Log.e("Auth", " Exception Message" + e.message.toString())
+            Utils.showErrorAlert(
+                activity,
+                "An error occurred while authenticating through saved credentials. Check logs for additional details"
+            )
+            return null
+        }
+        return null
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        configureProgress(View.INVISIBLE)
+        _binding = null
+    }
+
+    interface SignInFragmentCallback {
+        fun showHome()
+    }
+}
