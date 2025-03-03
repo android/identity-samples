732953f7fc214f6b6ca7d97a944a4a6bcd1877dc
Neelansh Sahai <neelanshsahai@google.com>
2025-03-03 18:27:26 +0530

    Fix race condition
    Change-Id: If64b5f6a76010e4a867dce4a8dca8b45e43a4728

--- Shrine/app/src/main/java/com/example/android/authentication/shrine/MainActivity.kt
+++ Shrine/app/src/main/java/com/example/android/authentication/shrine/MainActivity.kt
@@ -19,15 +19,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.android.authentication.shrine.ui.ShrineNavigation
-import com.example.android.authentication.shrine.ui.navigation.ShrineAppDestinations
 import com.example.android.authentication.shrine.ui.theme.ShrineTheme
 import com.example.android.authentication.shrine.ui.viewmodel.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -43,26 +40,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        installSplashScreen()
-
-        var startDestination = ShrineAppDestinations.AuthRoute.name
-        CoroutineScope(Dispatchers.Main).launch {
-            startDestination = if (splashViewModel.isSignedInThroughPassword()) {
-                if (splashViewModel.isSignedInThroughPasskeys()) {
-                    ShrineAppDestinations.MainMenuRoute.name
-                } else {
-                    ShrineAppDestinations.CreatePasskeyRoute.name
-                }
-            } else {
-                ShrineAppDestinations.AuthRoute.name
-            }
+        installSplashScreen().setKeepOnScreenCondition {
+            splashViewModel.uiState.value.isLoading
         }
 
         setContent {
             // Setting theme for the App
             ShrineTheme {
                 ShrineNavigation(
-                    startDestination = startDestination,
+                    startDestination = splashViewModel.uiState.collectAsState().value.nextScreen,
                     credentialManagerUtils = credentialManagerUtils,
                 )
             }
